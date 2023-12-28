675f9748964b527824462fe4e48a422a430930f2
송이영_201901962 <99798380+EeyoungSong@users.noreply.github.com>
2023-12-29 00:35:52 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 231229/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
+++ 231229/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
@@ -1,22 +1,33 @@
+# n = int(input())
+
+# def get_odd_sum(n):
+#     if n <= 0:
+#         return 0
+#     if n % 2 != 0:
+#         return get_odd_sum(n-1) + n
+#     else:
+#         return get_odd_sum(n-1) + 0
+
+# def get_even_sum(n):
+#     if n <= 0:
+#         return 0
+#     if n % 2 == 0:
+#         return get_even_sum(n-1) + n
+#     else:
+#         return get_even_sum(n-1) + 0
+
+# if n % 2 == 0:
+#     print(get_even_sum(n))
+# else:
+#     print(get_odd_sum(n))
+
 n = int(input())
 
-def get_odd_sum(n):
-    if n <= 0:
-        return 0
-    if n % 2 != 0:
-        return get_odd_sum(n-1) + n
-    else:
-        return get_odd_sum(n-1) + 0
+def get_num(n):
+    if n == 1:
+        return n
+    elif n == 2:
+        return n
+    return get_num(n-2) + n
 
-def get_even_sum(n):
-    if n <= 0:
-        return 0
-    if n % 2 == 0:
-        return get_even_sum(n-1) + n
-    else:
-        return get_even_sum(n-1) + 0
-
-if n % 2 == 0:
-    print(get_even_sum(n))
-else:
-    print(get_odd_sum(n))
+print(get_num(n))
