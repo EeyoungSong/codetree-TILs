e330f3aea0037d3127506a4c04e170717ed00aa0
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-01-02 00:08:16 +0900

    Title: Date to Date, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240102/Date to Date/date-to-date.py
@@ -0,0 +1,21 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+def calc_date(m1, d1, m2, d2):
+    num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    month = m1
+    date = d1
+    elapsed_time = 1
+    while True:
+        if month == m2 and date == d2 + 1:
+            break
+
+        elapsed_time += 1
+        date += 1
+
+        if date == num_of_days[month]:
+            month += 1
+            date = 1
+    
+    return elapsed_time
+
+print(calc_date(m1, d1, m2, d2))
