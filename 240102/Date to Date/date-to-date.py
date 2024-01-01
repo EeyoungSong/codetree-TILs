157aa444fee9230b159183af959ece35dba113b4
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-01-02 00:10:39 +0900

    Title: Date to Date, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240102/Date to Date/date-to-date.py
+++ 240102/Date to Date/date-to-date.py
@@ -6,13 +6,13 @@ def calc_date(m1, d1, m2, d2):
     date = d1
     elapsed_time = 1
     while True:
-        if month == m2 and date == d2 + 1:
+        if month == m2 and date == d2:
             break
-
+        #print(month, date, elapsed_time)
         elapsed_time += 1
         date += 1
 
-        if date == num_of_days[month]:
+        if date > num_of_days[month]:
             month += 1
             date = 1
     
