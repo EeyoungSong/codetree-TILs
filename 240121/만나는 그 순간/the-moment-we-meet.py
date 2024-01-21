133fbeaea0c5af8cd96df3fe80cbf6e4e65b32c7
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-01-21 21:47:32 +0900

    Title: 만나는 그 순간, Time: 122ms, Memory: 40MB, Status: Wrong Answer - CodeTree

--- 240121/만나는 그 순간/the-moment-we-meet.py
+++ 240121/만나는 그 순간/the-moment-we-meet.py
@@ -1,36 +1,26 @@
 MAX_NUM = 1000000
 n, m = map(int, input().split())
-arr_a = ['a'] * MAX_NUM
-arr_b = ['b'] * MAX_NUM
+arr_a = [0] * MAX_NUM
+arr_b = [0] * MAX_NUM
 
 def record(n, arr):
-    at = 0
-    a_dis = 0
+    at = 1
     for i in range(n):
         d, t = input().split()
         t = int(t)
-        if d == 'L':
-            for j in range(at, at + t):
-                arr[j] = a_dis
-                at += 1
-                a_dis -= 1
-        else:
-            for j in range(at, at + t):
-                arr[j] = a_dis
-                at += 1
-                a_dis += 1
+        for _ in range(t):
+            arr[at] = arr[at - 1] + (1 if d == 'R' else -1) 
+            at += 1
 
     return arr
 
 arr_a = record(n, arr_a)
 arr_b = record(m, arr_b)
 
-check = True
-for i in range(MAX_NUM):
+ans = -1
+for i in range(1, MAX_NUM):
     if (i != 0) and (arr_a[i] == arr_b[i]):
-        print(i)
-        check = False
+        ans = i
         break
 
-if check == True:
-    print(-1)
+print(ans)
