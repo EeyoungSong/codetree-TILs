5390acc54376f46dd1b1f2124dcdd0784c36971a
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-02-04 22:25:03 +0900

    Title: 행복한 수열의 개수, Time: 128ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240204/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240204/행복한 수열의 개수/number-of-happy-sequence.py
@@ -3,15 +3,23 @@ n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 ans = 0
 
+garo_num_list = []
+sero_num_list = []
+
 for i in range(n):
     p = grid[i][0]
     cnt = 1
     for j in range(1, n):
+        #print('p:', p)
         if p == grid[i][j]:
             cnt += 1
+        else:
+            cnt = 1
         p = grid[i][j]
+    #print(cnt)
     if cnt >= m:
-        ans += 1    
+        ans += 1
+        garo_num_list.append(i)   
 
 for j in range(n):
     p = grid[0][j]
@@ -19,9 +27,15 @@ for j in range(n):
     for i in range(1, n):
         if p == grid[i][j]:
             cnt += 1
+        else:
+            cnt = 1
         p = grid[i][j]
         #print(p)
+        
     if cnt >= m:
         ans += 1
+        sero_num_list.append(j)
 
 print(ans)
+# print(garo_num_list)
+# print(sero_num_list)
