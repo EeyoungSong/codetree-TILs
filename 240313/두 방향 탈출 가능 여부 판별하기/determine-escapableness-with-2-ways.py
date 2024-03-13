113e570010234cc1163f3daa464ecb73957c94d1
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-03-13 16:11:22 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240313/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240313/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -6,7 +6,7 @@ def in_range(r, c):
     return 0 <= r < m and 0 <= c < n
 
 def dfs(r, c):
-    for dr, dc in zip((0, 1), (1, 0)):
+    for dr, dc in zip((1, 0), (0, 1)):
         nr, nc = r + dr, c + dc 
         if in_range(nr, nc) and grid[nr][nc] != 0 and not visited[nr][nc]:
             visited[nr][nc] = 1
