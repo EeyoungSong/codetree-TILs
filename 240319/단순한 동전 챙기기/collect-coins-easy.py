66080a708fdb6de868bb4cd15df6802072d3e26a
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-03-19 17:55:47 +0900

    Title: 단순한 동전 챙기기, Time: 139ms, Memory: 26MB, Status: Passed - CodeTree

--- 240319/단순한 동전 챙기기/collect-coins-easy.py
+++ 240319/단순한 동전 챙기기/collect-coins-easy.py
@@ -3,17 +3,19 @@ import sys
 n = int(input())
 grid = [list(input()) for _ in range(n)]
 pos_dic = {}
+num_arr = []
 s = ('', '')
 e = ('', '')
 
 # 자료 정리
 for i in range(n):
     for j in range(n):
-        # if grid[i][j] == 'S':
-        #     s = (i, j)
-        # elif grid[i][j] == 'E':
-        #     e = (i, j)
-        if grid[i][j] != '.':
+        if grid[i][j] == 'S':
+            s = (i, j)
+        elif grid[i][j] == 'E':
+            e = (i, j)
+        elif grid[i][j] != '.':
+            num_arr.append(grid[i][j])
             pos_dic[grid[i][j]] = (i, j)
 
 #print(pos_dic)
@@ -25,33 +27,34 @@ chooesd_list = []
 
 def print_ans():
     l = []
-    for a in choosed:
-        l.append(a)
+    for i in range(len(choosed)):
+        if choosed[i] == 1:
+            l.append(num_arr[i])
     chooesd_list.append(l)
-    
+
 
 def choose(curr_num, cnt):
     if curr_num > m:
         if cnt == p:
             print_ans()
         return
-    choosed.append(curr_num)
+    choosed.append(1)
     choose(curr_num+1, cnt+1)
     choosed.pop()
 
+    choosed.append(0)
     choose(curr_num+1, cnt)
-    
+    choosed.pop()
+
 # 조합에 따른 거리 구하는 함수
 def cal_distance(arr):
-    # s2f = abs(pos_dic[arr[0]][0] - s[0]) + abs(pos_dic[arr[0]][1] - s[1])
-    # l2e = abs(pos_dic[arr[-1]][0] - e[0]) + abs(pos_dic[arr[-1]][1] - e[1])
+    s2f = abs(pos_dic[arr[0]][0] - s[0]) + abs(pos_dic[arr[0]][1] - s[1])
+    l2e = abs(pos_dic[arr[-1]][0] - e[0]) + abs(pos_dic[arr[-1]][1] - e[1])
 
-    t_dis = 0
-    arr.insert(0, 'S')
-    arr.append('E')
-    #print(arr)
+    t_dis = s2f + l2e
+
     for i in range(1, len(arr)):
-        #print(str(arr[i-1]))
+    #print(str(arr[i-1]))
         r1, c1 = pos_dic[str(arr[i-1])]
         r2, c2 = pos_dic[str(arr[i])]
         d = abs(r1 - r2) + abs(c1 - c2)
@@ -59,9 +62,10 @@ def cal_distance(arr):
     return t_dis
 
 
-# 3 ~ m 까지 p를 바꿔가면서 최솟값 구해야함 
+# 3 ~ m 까지 p를 바꿔가면서 최솟값 구해야함
 answer = -1
-m = len(pos_dic) - 2
+m = len(num_arr)
+num_arr.sort()
 for i in range(3, m+1):
     p = i
     choose(1, 0)
