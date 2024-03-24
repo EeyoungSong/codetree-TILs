1042c20a022b3b26ff23d219d67cd8737f19ba6c
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-03-25 03:19:01 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240325/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+arr = []
+for _ in range(n):
+    w, v = map(int, input().split())
+    arr.append((w, v, v/w))
+
+arr.sort(key=lambda x : x[2], reverse=True)
+
+w_sum = 0
+v_sum = 0
+idx = 0
+while True:
+    if (m - w_sum) < arr[idx][0]:
+        v_sum += arr[idx][1] * ((m - w_sum) / arr[idx][0])
+        print("%.3f" % v_sum)
+        break
+    w_sum += arr[idx][0]
+    v_sum += arr[idx][1]
+    idx += 1
