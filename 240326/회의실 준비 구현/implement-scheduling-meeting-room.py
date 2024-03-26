8806ed877748b4fd2665cc491f0f33acab9aca66
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-03-26 11:30:18 +0900

    Title: 회의실 준비 구현, Time: 371ms, Memory: 33MB, Status: Passed - CodeTree

--- 240326/회의실 준비 구현/implement-scheduling-meeting-room.py
+++ 240326/회의실 준비 구현/implement-scheduling-meeting-room.py
@@ -5,15 +5,16 @@ arr.sort(key=lambda x : x[1])
 
 used_arr = [0] * MAX_NUM
 
+
 cnt = 0
-for elem in arr:
-    empty = True
-    for i in range(elem[0], elem[1]):
-        if used_arr[i] == 1:
-            empty = False
-    if empty == True:
-        cnt += 1
-        for i in range(elem[0], elem[1]):
-            used_arr[i] = 1
+m = 0
+for i in range(0, len(arr)):
+    if m > arr[i][0]:
+        continue
+    cnt += 1
+    m = max(arr[i][1], m)
+    #print(arr[i])
+    for i in range(arr[i][0], arr[i][1]):
+        used_arr[i] = 1
 
 print(cnt)
