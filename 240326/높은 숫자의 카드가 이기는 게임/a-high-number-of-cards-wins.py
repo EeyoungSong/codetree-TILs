16da03c8142e9d923e9b24c3844b4431b13621bc
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-03-26 16:53:29 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240326/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -0,0 +1,13 @@
+n = int(input())
+b_arr = [int(input()) for _ in range(n)]
+b_arr.sort()
+a_arr = [x for x in list(range(1, 2*n + 1)) if x not in b_arr]
+a_arr.sort()
+
+point = 0
+for b in b_arr:
+    for a in a_arr:
+        if b < a:
+            point += 1
+            break
+print(point)
