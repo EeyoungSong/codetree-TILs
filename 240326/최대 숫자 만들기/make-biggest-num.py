1a61fd12dc3d6c82236bb75b9a43606f9fdd1c02
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-03-26 15:53:48 +0900

    Title: 최대 숫자 만들기, Time: 283ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240326/최대 숫자 만들기/make-biggest-num.py
+++ 240326/최대 숫자 만들기/make-biggest-num.py
@@ -1,18 +1,20 @@
 from functools import cmp_to_key
 
 def compare(x, y):
-    if len(y) == 1:
+    if len(x) == 1:
+        # print(x)
+        # print(x+y, y+x)
         if x + y > y + x:
             return -1
         else:
             return 1
     # x가 y보다 크다면
     # 우리가 원하는 방향입니다.
-    if x > y:
+    elif x > y:
         return -1
     # y가 x보다 크다면
     # 우리가 원하지 않는 방향입니다.
-    if y > x:
+    elif y > x:
         return 1
     # 우선 순위가 동일한 경우입니다.
     return 0
@@ -31,4 +33,5 @@ arr.sort(reverse=True)
 arr.sort(key=cmp_to_key(compare))
 
 
-print(arr)
+for elem in arr:
+    print(elem, end="")
