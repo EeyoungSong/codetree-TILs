1f5c9a6ea11b6ea357420a637ffd311d80f37f0c
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-03-27 10:49:31 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240327/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 240327/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -1,8 +1,14 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-min_num = min(arr)
-idx = arr.index(min_num)
-max_num = max(arr[idx:])
+# min_num = min(arr)
+# idx = arr.index(min_num)
+# max_num = max(arr[idx:])
+
+# print(max_num - min_num)
+
+max_num = max(arr)
+idx = arr.index(max_num)
+min_num = min(arr[:idx+1])
 
 print(max_num - min_num)
