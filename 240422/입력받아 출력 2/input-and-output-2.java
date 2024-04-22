7d1292c73a38bec7b56c5de9b8dfe0e22f7c1b7a
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-04-22 16:25:50 +0900

    Title: 입력받아 출력 2, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240422/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan = new Scanner(System.in);
+        scan.useDelimiter("-");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.print(a);
+        System.out.print(b);
+
+    }
+}
