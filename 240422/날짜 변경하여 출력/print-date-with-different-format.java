c7bf9e4b986eb4a9cfd47f1407577e3f3fb646c5
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-04-22 16:33:15 +0900

    Title: 날짜 변경하여 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240422/날짜 변경하여 출력/print-date-with-different-format.java
+++ 240422/날짜 변경하여 출력/print-date-with-different-format.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        System.out.printf("이것을\r덮어 씁니다. \n");
+        System.out.printf("이것을\r덮어 씁니다.\b \n");
+        float a = 1.09;
+        System.out.print(a);
+
     }
 }
