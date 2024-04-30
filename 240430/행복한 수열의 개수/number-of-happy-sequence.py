9bbe1ccbb4051ba1db8aa2cf053ea2f263ecf667
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-04-30 15:38:53 +0900

    Title: 행복한 수열의 개수, Time: 170ms, Memory: 25MB, Status: Passed - CodeTree

--- 240430/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240430/행복한 수열의 개수/number-of-happy-sequence.py
@@ -3,20 +3,17 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 ans = 0
 for i in range(n):
-    max_cnt = 0
     cnt = 1
     for j in range(1, n):
-        if grid[i][j-1] == grid[i][j]:
-            cnt += 1            
+        if grid[i][j] == grid[i][j-1]:
+            cnt += 1
         else:
             cnt = 1
         if cnt >= m:
             ans += 1
             break
 
-
 for j in range(n):
-    max_cnt = 0
     cnt = 1
     for i in range(1, n):
         if grid[i][j] == grid[i-1][j]:
@@ -26,6 +23,8 @@ for j in range(n):
         if cnt >= m:
             ans += 1
             break
-
+            
+if n == 1 and m == 1:
+    ans = 2
 
 print(ans)
