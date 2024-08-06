d55ba92c0de497271195030a0f801dff7be658a1
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-08-07 00:14:50 +0800

    Title: 가장 짧은 부분합, Time: 155ms, Memory: 32MB, Status: Passed - CodeTree

--- 240807/가장 짧은 부분합/shortest-subtotal.py
+++ 240807/가장 짧은 부분합/shortest-subtotal.py
@@ -1,16 +1,26 @@
 import sys
+INT_MAX = sys.maxsize
 
-n, s = map(int, input().split())
-arr = list(map(int, input().split()))
+n, s = tuple(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
 
-ans = sys.maxsize
+ans = INT_MAX
 
+sum_val = 0
 j = 0
-for i in range(n):
-    sum_val = 0
-    for j in range(i, n):
-        sum_val += arr[j]
-        if sum_val >= s:
-            ans = min(ans, j-i+1)
-          
+for i in range(1, n + 1):
+    while j + 1 <= n and sum_val < s:
+        sum_val += arr[j + 1]
+        j += 1
+    
+    if sum_val < s:
+        break
+    
+    ans = min(ans, j - i + 1)
+
+    sum_val -= arr[i]
+
+if ans == INT_MAX:
+    ans = -1
+
 print(ans)
