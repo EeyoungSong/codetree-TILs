2e1967741791dd1e1ddb30e8ae0bd1b7c767207d
송이영 <99798380+EeyoungSong@users.noreply.github.com>
2024-08-08 22:50:41 +0800

    Title: 겹치는 숫자가 없는 최대 구간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240808/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.py
+++ 240808/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.py
@@ -5,7 +5,7 @@ count_arr = [0] * len(set(arr))
 ans = 0
 j = 0
 for i in range(1, n+1):
-    while j + 1 <= n and count_arr[arr[j+1]] == 0:
+    while j + 1 <= n and count_arr[arr[j+1]] < 1:
         count_arr[arr[j+1]] += 1
         j += 1
     
